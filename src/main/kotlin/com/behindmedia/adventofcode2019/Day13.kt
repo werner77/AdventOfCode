1b7a623c36c0b0b82db7d691a5c617ce3b6f5841
Werner Altewischer <werner.altewischer@ing.com>
2019-12-13 08:33:23 +0100

    Refactoring

--- src/main/kotlin/com/behindmedia/adventofcode2019/Day13.kt
+++ src/main/kotlin/com/behindmedia/adventofcode2019/Day13.kt
@@ -1,8 +1,111 @@
 package com.behindmedia.adventofcode2019
 
+import java.lang.StringBuilder
+
 class Day13 {
 
-    fun execute() {
+    enum class Tile(val rawValue: Long) {
+        Empty(0L), Wall(1L), Block(2L), Paddle(3L), Ball(4L);
 
+        companion object {
+            fun from(rawValue: Long): Tile {
+                return values().first { rawValue == it.rawValue }
+            }
+        }
+    }
+
+    class GameState {
+        var ballPosition = Coordinate.origin
+        var paddlePosition = Coordinate.origin
+        var score = 0L
+        val map = mutableMapOf<Coordinate, Tile>()
+
+        fun parseOutputs(outputs: List<Long>) {
+            var i = 0
+            while (i < outputs.size) {
+                val x = outputs[i]
+                val y = outputs[i + 1]
+                val tileValue = outputs[i + 2]
+
+                if (x == -1L && y == 0L) {
+                    //score
+                    score = tileValue
+                } else {
+                    val tile = Tile.from(tileValue)
+                    val coordinate = Coordinate(x.toInt(), y.toInt())
+                    map[coordinate] = tile
+
+                    when (tile) {
+                        Tile.Ball -> ballPosition = coordinate
+                        Tile.Paddle -> paddlePosition = coordinate
+                    }
+                }
+                i += 3
+            }
+        }
+
+        override fun toString(): String {
+            val buffer = StringBuilder()
+            val coordinates = map.keys
+            val range = coordinates.range()
+
+            buffer.append("Score: $score\n\n")
+
+            for (coordinate in range) {
+                val value = map[coordinate] ?: throw IllegalStateException("No tile found for $coordinate")
+
+                val s = when (value) {
+                    Tile.Empty -> " "
+                    Tile.Wall -> "X"
+                    Tile.Block -> "\u25A1"
+                    Tile.Paddle -> "="
+                    Tile.Ball -> "O"
+                }
+                buffer.append(s)
+                if (coordinate.x == range.endInclusive.x) {
+                    buffer.append("\n")
+                }
+            }
+            return buffer.toString()
+        }
+    }
+
+    fun numberOfBlocks(program: String): Int {
+        val computer = Computer(program)
+        val result = computer.process()
+        assert(result.status == Computer.Status.Finished)
+
+        val gameState = GameState()
+        gameState.parseOutputs(result.outputs)
+
+        return gameState.map.values.count { it == Tile.Block }
+    }
+
+    fun play(program: String, onUpdate: (GameState) -> Unit): GameState {
+        val decodedProgram = Computer.parseEncodedState(program).toMutableList()
+        decodedProgram[0] = 2
+        val computer = Computer(decodedProgram)
+        var input: Long? = null
+
+        val gameState = GameState()
+
+        while (computer.status != Computer.Status.Finished) {
+            val inputs = if (input == null) listOf() else listOf(input)
+
+            val result = computer.process(inputs)
+            val outputs = result.outputs
+
+            gameState.parseOutputs(outputs)
+
+            onUpdate(gameState)
+
+            input = when {
+                gameState.paddlePosition.x < gameState.ballPosition.x -> 1
+                gameState.paddlePosition.x > gameState.ballPosition.x -> -1
+                else -> 0
+            }
+        }
+
+        return gameState
     }
 }
--- src/test/kotlin/com/behindmedia/adventofcode2019/Day13Test.kt
+++ src/test/kotlin/com/behindmedia/adventofcode2019/Day13Test.kt
@@ -1,173 +1,69 @@
 package com.behindmedia.adventofcode2019
 
 import org.junit.Test
-import java.lang.StringBuilder
+import java.awt.Font
 import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.JTextArea
+import kotlin.test.assertEquals
 
 
 class Day13Test {
 
-    class GameState {
-        var ballPosition = Coordinate.origin
-        var paddlePosition = Coordinate.origin
-        var ballDirection = Coordinate(0, 1)
-        var score = 0L
-        val map = mutableMapOf<Coordinate, Tile>()
-
-        fun parseOutputs(outputs: List<Long>) {
-            var i = 0
-            while (i < outputs.size) {
-                val x = outputs[i]
-                val y = outputs[i + 1]
-                val tileValue = outputs[i + 2]
-
-                if (x == -1L && y == 0L) {
-                    //score
-                    score = tileValue
-                } else {
-                    val tile = Tile.from(tileValue)
-                    val coordinate = Coordinate(x.toInt(), y.toInt())
-                    map[coordinate] = tile
-
-                    when (tile) {
-                        Tile.Ball -> ballPosition = coordinate
-                        Tile.Paddle -> paddlePosition = coordinate
-                    }
-                }
-                i += 3
-            }
-        }
-
-        fun printMap(): String {
-
-            val buffer = StringBuilder()
-            val coordinates = map.keys
-            val range = coordinates.range()
-
-            for (coordinate in range) {
-                val value = map[coordinate] ?: throw IllegalStateException("No tile found for $coordinate")
-
-                val s = when (value) {
-                    Tile.Empty -> " "
-                    Tile.Wall -> "X"
-                    Tile.Block -> "B"
-                    Tile.Paddle -> "="
-                    Tile.Ball -> "O"
-                }
-                buffer.append(s)
-                if (coordinate.x == range.endInclusive.x) {
-                    buffer.append("\n")
-                }
-            }
-            return buffer.toString()
-        }
-    }
-
     @Test
-    fun execute() {
+    fun puzzle1() {
+        val day13 = Day13()
+        val program = read("/day13.txt")
+        val result = day13.numberOfBlocks(program)
 
-        val input = read("/day13.txt")
-        val computer = Computer(input)
-
-        var numberOfTiles = 0
-
-        while (true) {
-            val result = computer.process()
-
-            for (i in 2 until result.outputs.size step 3) {
-                if (result.outputs[i] == 2L) {
-                    numberOfTiles++
-                }
-            }
-
-            if (result.status == Computer.Status.Finished) {
-                break
-            }
-        }
-
-        println(numberOfTiles)
+        println(result)
+        assertEquals(335, result)
     }
 
-    enum class Tile(val rawValue: Long) {
-        Empty(0L), Wall(1L), Block(2L), Paddle(3L), Ball(4L);
 
-        companion object {
-            fun from(rawValue: Long): Tile {
-                return values().first { rawValue == it.rawValue }
-            }
-        }
-    }
 
     @Test
     fun puzzle2() {
+        val visualize = false
+        val program = read("/day13.txt")
+        val day13 = Day13()
+        var gui: TextWindow? = null
 
-        val program = Computer.parseEncodedState(read("/day13.txt")).toMutableList()
-
-        program[0] = 2L
-
-        val computer = Computer(program)
-        val inputs = mutableListOf<Long>()
-
-        val gameState = GameState()
-
-        val textWindow = TextWindow()
-
-        while (true) {
-            val result = computer.process(inputs)
-            val outputs = result.outputs
-
-            gameState.parseOutputs(outputs)
-
-            textWindow.setText(gameState.printMap())
-
-            Thread.sleep(1000)
-
-            // Predict the impact position of the ball and move the paddle there as fast as possible
-            inputs.clear()
-
-            if (gameState.paddlePosition.x < gameState.ballPosition.x) {
-                inputs.add(1)
-            } else if (gameState.paddlePosition.x > gameState.ballPosition.x) {
-                inputs.add(-1)
-            } else {
-                inputs.add(0)
-            }
-
-            if (result.status == Computer.Status.Finished) {
-                break
-            }
+        if (visualize) {
+            gui = TextWindow()
         }
 
-        gameState.printMap()
-        println("Score = ${gameState.score}")
-
-        println("Done")
+        val finalGameState = day13.play(program) { gameState ->
+            if (visualize) {
+                gui?.setText(gameState.toString())
+                Thread.sleep(1000 / 25)
+            }
+        }
+        println(finalGameState.score)
+        assertEquals(15706, finalGameState.score)
     }
 
+    private class TextWindow() {
 
-}
+        private val theText: JTextArea
 
-class TextWindow() {
+        init {
+            val theFrame = JFrame()
+            theFrame.title = "AdventOfCode Day 13"
+            theFrame.setSize(300, 300)
+            theFrame.setLocation(550, 400)
+            val mainPanel = JPanel()
+            theText = JTextArea(40, 40) //create the text area
+            theText.font = Font("monospaced", Font.PLAIN, 12)
+            mainPanel.add(theText) //add the text area to the panel
+            theFrame.contentPane.add(mainPanel) //add the panel to the frame
+            theFrame.pack()
+            theFrame.isVisible = true
+        }
 
-    private val theText: JTextArea
-
-    init  //constructor of the DisplayGuiHelp object that has the list passed to it on creation
-    {
-        val theFrame = JFrame()
-        theFrame.title = "AdventOfCode Day 13"
-        theFrame.setSize(500, 500)
-        theFrame.setLocation(550, 400)
-        val mainPanel = JPanel()
-        theText = JTextArea(5, 25) //create the text area
-        mainPanel.add(theText) //add the text area to the panel
-        theFrame.contentPane.add(mainPanel) //add the panel to the frame
-        theFrame.pack()
-        theFrame.isVisible = true
+        fun setText(string: String) {
+            theText.text = string
+        }
     }
 
-    fun setText(string: String) {
-        theText.insert(string, 0)
-    }
 }
