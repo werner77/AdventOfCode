2dff5bcdd179e17c2472db2691bec5dcb159ce34
Werner Altewischer <werner.altewischer@ing.com>
2020-12-07 11:39:24 +0100

    Day 7

--- src/main/kotlin/com/behindmedia/adventofcode/year2020/Day7.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2020/Day7.kt
@@ -26,22 +26,54 @@ class Day7 {
         }
 
         val found = mutableSetOf<String>()
-        traverse(map, startBag, found)
+        traverse1(map, startBag, found)
         return found.size
     }
 
     fun part2(input: String, startBag: String = "shiny gold"): Int {
-        return 0
+        val map = mutableMapOf<String, MutableList<Pair<String, Int>>>()
+        parseLines(input) {
+            // posh lavender bags contain 5 striped silver bags, 3 wavy beige bags, 3 dim brown bags, 5 clear indigo bags.
+            val regex1 = Regex("""([a-z ]+) bag[s]? contain""")
+            val regex2 = Regex("""([0-9]+) ([a-z ]+) bag[s]?""")
+
+            val matchResult1 = regex1.find(it) ?: error("Could not find match")
+
+            val key = matchResult1.groupValues[1].trim()
+            val values = map.getOrPut(key) { mutableListOf() }
+
+            var pos = matchResult1.range.last + 1
+            while (pos < it.length) {
+                val matchResult2 = regex2.find(it, pos) ?: break
+                val count = matchResult2.groupValues[1].toInt()
+                val value = matchResult2.groupValues[2].trim()
+                values.add(Pair(value, count))
+                pos = matchResult2.range.last + 1
+            }
+        }
+
+        val cache = mutableMapOf<String, Int>()
+        return traverse2(map, startBag, cache) - 1
     }
 
-    private fun traverse(map: Map<String, List<Pair<String, Int>>>, bag: String, found: MutableSet<String>) {
+    private fun traverse1(map: Map<String, List<Pair<String, Int>>>, bag: String, found: MutableSet<String>) {
         val list = map[bag] ?: return
         for (containingBag in list) {
             if (!found.contains(containingBag.first)) {
                 found.add(containingBag.first)
-                traverse(map, containingBag.first, found)
+                traverse1(map, containingBag.first, found)
             }
         }
+    }
+
+    private fun traverse2(map: Map<String, List<Pair<String, Int>>>, bag: String, cache: MutableMap<String, Int>): Int {
+        val list = map[bag] ?: emptyList()
+        var count = 1
+        list.forEach {
+            val bagCount = cache.getOrPut(it.first) { traverse2(map, it.first, cache) }
+            count += it.second * bagCount
+        }
+        return count
     }
 }
 
