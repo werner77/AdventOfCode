91b5571133ecb7949a010a0efb390dd8981ef1da
Werner Altewischer <werner.altewischer@ing.com>
2020-12-14 10:42:14 +0100

    2020 day 14

---
+++ src/main/kotlin/com/behindmedia/adventofcode/year2020/Day14.kt
@@ -0,0 +1,88 @@
+package com.behindmedia.adventofcode.year2020
+
+import com.behindmedia.adventofcode.common.parseLines
+
+class Day14 {
+
+    fun part1(input: String): Long {
+        return parse(input, this::process1)
+    }
+
+    fun part2(input: String): Long {
+        return parse(input, this::process2)
+    }
+
+    private fun process1(mask: String, address: Long, value: Long, values: MutableMap<Long, Long>) {
+        assert(mask.length == 36)
+        var currentValue = value
+        for (i in 0 until 36) {
+            val m = (1L shl i)
+            if (mask[35 - i] == '0') {
+                // Set this bit to 0
+                currentValue = currentValue and m.inv()
+            } else if (mask[35 - i] == '1') {
+                // Set this bit to 1
+                currentValue = currentValue or m
+            }
+        }
+        values[address] = currentValue
+    }
+
+    private fun process2(mask: String, address: Long, value: Long, values: MutableMap<Long, Long>) {
+        assert(mask.length == 36)
+        var modifiedAddress = address
+        val floatingBits = mutableListOf<Int>()
+        for (i in 0 until 36) {
+            val j = 35 - i
+            if (mask[j] == '1') {
+                // set bit to one
+                modifiedAddress = modifiedAddress or (1L shl i)
+            } else if (mask[j] == 'X') {
+                // float
+                floatingBits.add(i)
+            }
+        }
+        permutate(modifiedAddress, value, floatingBits, values)
+    }
+
+    private fun parse(input: String, process: (String, Long, Long, MutableMap<Long, Long>) -> Unit): Long {
+        val maskRegex = Regex("""^mask = ([0-1X]+)$""")
+        val memRegex = Regex("""^mem\[([0-9]+)\] = ([0-9]+)$""")
+        var currentMask = ""
+        val state = mutableMapOf<Long, Long>()
+
+        parseLines(input) { line ->
+
+            val maskMatch = maskRegex.matchEntire(line)
+            val memMatch = memRegex.matchEntire(line)
+
+            if (maskMatch != null) {
+                currentMask = maskMatch.groupValues[1]
+            } else if (memMatch != null) {
+                val address = memMatch.groupValues[1].toLong()
+                val value = memMatch.groupValues[2].toLong()
+                process(currentMask, address, value, state)
+            } else {
+                error("No match found for line: $line")
+            }
+        }
+        return state.values.sum()
+    }
+
+    private fun permutate(address: Long, value: Long, bits: List<Int>, values: MutableMap<Long, Long>) {
+        if (bits.isEmpty()) {
+            values[address] = value
+            return
+        }
+
+        val bit = bits.first()
+        val newBits = bits.drop(1)
+
+        val mask = 1L shl bit
+
+        val address1 = address or mask
+        val address2 = address and mask.inv()
+        permutate(address1, value, newBits, values)
+        permutate(address2, value, newBits, values)
+    }
+}
