782ac9779592e8754d021e0c5022c8d56cd041c4
Werner Altewischer <werner.altewischer@gmail.com>
2020-12-23 10:45:53 +0100

    Day 23 improved implementations avoiding unnecessary allocations

--- src/main/kotlin/com/behindmedia/adventofcode/year2020/Day23.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2020/Day23.kt
@@ -2,7 +2,7 @@ package com.behindmedia.adventofcode.year2020
 
 class Day23 {
 
-    data class Cup(val values: List<Int>) {
+    class Cup(values: List<Int>) {
 
         /**
          * Single linked list
@@ -21,38 +21,38 @@ class Day23 {
          * Map to quickly find nodes by value
          */
         private val lookupMap: Map<Int, Node>
-        var currentNode: Node
-            private set
-        val size: Int
-            get() = values.size
+        private var currentNode: Node
+        private val size: Int = values.size
         private val minValue = values.minOrNull() ?: error("Expected at least one value to be present")
         private val maxValue = values.maxOrNull() ?: error("Expected at least one value to be present")
+        private val pickedUpNodeValues = IntArray(3)
+
         private fun node(value: Int): Node {
             return lookupMap[value] ?: error("Could find no node with the specified value")
         }
 
         init {
-            var firstNode: Node? = null
-            var lastNode: Node? = null
-            val map = HashMap<Int, Node>(size, 1.0f)
-            for (value in values) {
-                val node = Node(value, null)
-                if (firstNode == null) {
-                    firstNode = node
+            lookupMap = HashMap<Int, Node>(size, 1.0f).also { map ->
+                var firstNode: Node? = null
+                var lastNode: Node? = null
+                for (value in values) {
+                    val node = Node(value, null)
+                    if (firstNode == null) {
+                        firstNode = node
+                    }
+                    map[value] = node
+                    lastNode?.next = node
+                    lastNode = node
                 }
-                map[value] = node
-                lastNode?.next = node
-                lastNode = node
+                lastNode?.next = firstNode
+                currentNode = firstNode ?: error("Expected at least one node to be present")
             }
-            lastNode?.next = firstNode
-            currentNode = firstNode!!
-            lookupMap = map
         }
 
-        fun toList(fromValue: Int? = null, inclusive: Boolean = true, count: Int = size): List<Int> {
+        fun toList(fromValue: Int? = null, inclusive: Boolean = true, count: Int = size - 1): List<Int> {
             return ArrayList<Int>(size).apply {
                 var node = fromValue?.let { node(it) } ?: currentNode
-                repeat(count) {
+                repeat(count + 1) {
                     if (it > 0 || inclusive) {
                         add(node.value)
                     }
@@ -62,28 +62,27 @@ class Day23 {
         }
 
         fun play() {
-            val pickedUpNodes = ArrayList<Node>(3)
-            val pickedUpValues = HashSet<Int>(3, 1.0f)
             var nextNode = currentNode.requireNext()
+            val firstPickedUpNode = nextNode
+            var lastPickedUpNode = firstPickedUpNode
             var targetValue = currentNode.value - 1
-            repeat(3) {
-                pickedUpValues.add(nextNode.value)
-                pickedUpNodes.add(nextNode)
+            repeat(3) { index ->
+                pickedUpNodeValues[index] = nextNode.value
+                lastPickedUpNode = nextNode
                 nextNode = nextNode.requireNext()
             }
             currentNode.next = nextNode
-            pickedUpNodes.last().next = null
-            while (pickedUpValues.contains(targetValue) || targetValue < minValue) {
+            while (targetValue < minValue || pickedUpNodeValues.contains(targetValue)) {
                 if (targetValue < minValue) {
                     targetValue = maxValue
                 } else {
                     targetValue--
                 }
             }
-            val findNode = node(targetValue)
-            val next = findNode.next
-            findNode.next = pickedUpNodes.first()
-            pickedUpNodes.last().next = next
+            val targetNode = node(targetValue)
+            val next = targetNode.next
+            targetNode.next = firstPickedUpNode
+            lastPickedUpNode.next = next
             currentNode = currentNode.requireNext()
         }
     }
@@ -107,6 +106,6 @@ class Day23 {
         repeat(10_000_000) {
             cup.play()
         }
-        return cup.toList(fromValue = 1, inclusive = false, count = 3).map { it.toLong() }.reduce { acc, i -> acc * i }
+        return cup.toList(fromValue = 1, inclusive = false, count = 2).map { it.toLong() }.reduce { acc, i -> acc * i }
     }
 }
