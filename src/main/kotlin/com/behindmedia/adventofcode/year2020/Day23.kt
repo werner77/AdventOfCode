b2f5cf4add581f051ac30894c2d53d6cfc92ce8c
Werner Altewischer <werner.altewischer@ing.com>
2020-12-23 07:50:12 +0100

    Day 23 preparations

---
+++ src/main/kotlin/com/behindmedia/adventofcode/year2020/Day23.kt
@@ -0,0 +1,11 @@
+package com.behindmedia.adventofcode.year2020
+
+class Day23 {
+    fun part1(input: String) : Long {
+        return 0
+    }
+
+    fun part2(input: String) : Long {
+        return 0
+    }
+}
---
+++ src/test/kotlin/com/behindmedia/adventofcode/year2020/Day23Test.kt
@@ -0,0 +1,18 @@
+package com.behindmedia.adventofcode.year2020
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class Day23Test {
+
+    @Test
+    fun part1() {
+        println(Day23().part1("/2020/day23.txt"))
+    }
+
+    @Test
+    fun part2() {
+        println(Day23().part2("/2020/day23.txt"))
+    }
+}
