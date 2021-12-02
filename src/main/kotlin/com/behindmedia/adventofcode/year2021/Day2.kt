9fa351846aa5d7f4147669abe565a2c7764018d5
Werner Altewischer <werner.altewischer@gmail.com>
2021-12-02 06:15:40 +0100

    2021 day2

---
+++ src/main/kotlin/com/behindmedia/adventofcode/year2021/Day2.kt
@@ -0,0 +1,54 @@
+package com.behindmedia.adventofcode.year2021
+
+import com.behindmedia.adventofcode.common.*
+
+private fun part1() {
+    var x = 0L
+    var y = 0L
+    parseLines("/2021/day2.txt") {
+        val components = it.split(" ")
+        val amount = components[1].toLong()
+        when (components[0]) {
+            "forward" -> {
+                x += amount
+            }
+            "up" -> {
+                y -= amount
+            }
+            "down" -> {
+                y += amount
+            }
+            else -> error("Parse error")
+        }
+    }
+    println(x * y)
+}
+
+private fun part2() {
+    var aim = 0L
+    var x = 0L
+    var y = 0L
+    parseLines("/2021/day2.txt") {
+        val components = it.split(" ")
+        val amount = components[1].toLong()
+        when (components[0]) {
+            "forward" -> {
+                x += amount
+                y += aim * amount
+            }
+            "up" -> {
+                aim -= amount
+            }
+            "down" -> {
+                aim += amount
+            }
+            else -> error("Parse error")
+        }
+    }
+    println(x * y)
+}
+
+fun main() {
+    part1()
+    part2()
+}
