74144fc63c9229f0b5320a32095e0f73d67c4788
Werner Altewischer <werner.altewischer@gmail.com>
2023-12-23 19:27:17 +0100

    Code simplification

--- src/main/kotlin/com/behindmedia/adventofcode/year2023/day23/Day23.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2023/day23/Day23.kt
@@ -2,76 +2,84 @@ package com.behindmedia.adventofcode.year2023.day23
 
 import com.behindmedia.adventofcode.common.CharGrid
 import com.behindmedia.adventofcode.common.Coordinate
-import com.behindmedia.adventofcode.common.Path
 import com.behindmedia.adventofcode.common.read
+import com.behindmedia.adventofcode.common.timing
 import kotlin.math.max
 
 fun main() {
     val grid = CharGrid(read("/2023/day23.txt"))
-
     val start = grid.single { it.key.y == 0 && it.value == '.' }.key
     val finish = grid.single { it.key.y == grid.maxY && it.value == '.' }.key
 
-    // Part 1, direct implementation
-    println(part1(grid, start, finish))
+    timing {
+        // Part 1
+        println(solution(grid, start, finish, true))
 
-    // Part 2, weighted graph
-    println(part2(grid, start, finish))
+        // Part 2
+        println(solution(grid, start, finish, false))
+    }
 }
 
-private fun part2(grid: CharGrid, start: Coordinate, finish: Coordinate): Int {
+private fun solution(grid: CharGrid, start: Coordinate, finish: Coordinate, strict: Boolean): Int {
     // Determine start and finish point
-
-    val graph = findWeightedGraph(grid, start, finish)
-
+    val vertices = findVertices(grid, start, finish)
+    val edges = findEdges(grid, vertices, strict)
     // Process the connections with a dfs to find the max path
-    return findMaxSteps(graph, start, finish, mutableSetOf())
+    return findMaxSteps(edges, start, finish, mutableSetOf())
 }
 
-private fun findWeightedGraph(
+private fun findVertices(grid: CharGrid, start: Coordinate, finish: Coordinate): Set<Coordinate> {
+    val nodes = mutableSetOf<Coordinate>()
+    nodes += start
+    nodes += finish
+    val pending = ArrayDeque<Coordinate>()
+    pending += start
+    val seen = mutableSetOf<Coordinate>()
+    while (pending.isNotEmpty()) {
+        val current = pending.removeFirst()
+        if (current in seen) continue
+        seen += current
+        val candidates = current.directNeighbours.filter { grid.isReachable(it, it - current, false) }
+        if (candidates.size >= 3) {
+            nodes += current
+        }
+        pending += candidates
+    }
+    return nodes
+}
+
+private fun findEdges(
     grid: CharGrid,
-    start: Coordinate,
-    finish: Coordinate,
+    vertices: Set<Coordinate>,
+    strict: Boolean
 ): Map<Coordinate, List<Pair<Coordinate, Int>>> {
     val result = mutableMapOf<Coordinate, MutableList<Pair<Coordinate, Int>>>()
-    val pendingNodes = ArrayDeque<Coordinate>()
-    pendingNodes += start
-    pendingNodes += finish
-    while (pendingNodes.isNotEmpty()) {
-        val currentNode = pendingNodes.removeFirst()
-        if (result.contains(currentNode)) continue
-        val currentNodeList = mutableListOf<Pair<Coordinate, Int>>()
-        result[currentNode] = currentNodeList
-
-        // Find all reachable nodes from the current node, stop on any node
-        val pending = ArrayDeque<Path<Coordinate>>()
+    for (vertex in vertices) {
+        val currentEdges = mutableListOf<Pair<Coordinate, Int>>()
+        result[vertex] = currentEdges
+        val pending = ArrayDeque<Pair<Coordinate, Int>>()
+        pending += vertex to 0
         val seen = mutableSetOf<Coordinate>()
-        pending += Path(currentNode, 0, null)
         while (pending.isNotEmpty()) {
-            val path = pending.removeFirst()
-            val pos = path.destination
-            val length = path.length
-            if (pos in seen) continue
-            seen += pos
-            val lastDirection = path.parent?.let { pos - it.destination }
-            val candidates = pos.directNeighbours.filter { grid.isReachable(it, it - pos, false) && pos - it != lastDirection }
-            if (pos != currentNode && (candidates.size > 1 || pos == start || pos == finish)) {
-                // We found a node
-                currentNodeList += pos to length
-                pendingNodes += pos
+            val (pos, length) = pending.removeFirst()
+            if (pos != vertex && pos in vertices) {
+                // Add to edges
+                currentEdges += pos to length
                 continue
             }
+            if (pos in seen) continue
+            seen += pos
+            val candidates = pos.directNeighbours.filter { grid.isReachable(it, it - pos, strict) }
             for (next in candidates) {
-                pending += Path(next, length + 1, path)
+                pending += next to length + 1
             }
         }
     }
-    return result.also { println(it) }
+    return result
 }
 
-
 private fun findMaxSteps(
-    connections: Map<Coordinate, List<Pair<Coordinate, Int>>>,
+    graph: Map<Coordinate, List<Pair<Coordinate, Int>>>,
     current: Coordinate,
     to: Coordinate,
     seen: MutableSet<Coordinate>
@@ -83,62 +91,24 @@ private fun findMaxSteps(
         // Cannot reach same coordinate twice
         return -1
     }
-
-    // Mark as seen
     seen += current
     var maxSteps = -1
-    for ((neighbour, length) in connections.getOrDefault(current, emptyList())) {
-        val steps = findMaxSteps(connections, neighbour, to, seen).takeIf { it >= 0 } ?: continue
+    for ((neighbour, length) in graph[current]!!) {
+        val steps = findMaxSteps(graph, neighbour, to, seen).takeIf { it >= 0 } ?: continue
         maxSteps = max(maxSteps, steps + length)
     }
-    // Unwind
     seen -= current
     return maxSteps
 }
 
-private fun part1(
-    grid: CharGrid,
-    current: Coordinate,
-    to: Coordinate,
-    seen: MutableSet<Coordinate> = mutableSetOf()
-): Int {
-    if (current == to) {
-        // Found destination
-        return 0
-    } else if (current in seen) {
-        // Cannot reach same coordinate twice
-        return -1
+private fun CharGrid.isReachable(coordinate: Coordinate, direction: Coordinate, strict: Boolean): Boolean {
+    return when (val value = this.getOrNull(coordinate)) {
+        null, '#' -> false
+        '.' -> true
+        '>' -> !strict || direction == Coordinate.right
+        '<' -> !strict || direction == Coordinate.left
+        'v' -> !strict || direction == Coordinate.down
+        '^' -> !strict || direction == Coordinate.up
+        else -> error("Invalid value: $value")
     }
-
-    // Mark as seen
-    seen += current
-    var maxSteps = -1
-    for (neighbour in current.directNeighbours) {
-        if (!grid.isReachable(neighbour, neighbour - current, true)) continue
-        val steps = part1(grid, neighbour, to, seen).takeIf { it >= 0 } ?: continue
-        maxSteps = max(maxSteps, steps + 1)
-    }
-    // Unwind
-    seen -= current
-    return maxSteps
-}
-
-private fun CharGrid.isReachable(coordinate: Coordinate, direction: Coordinate?, strict: Boolean): Boolean {
-    val value = this.getOrNull(coordinate) ?: return false
-    if (value == '#') {
-        return false
-    } else if (value != '.' && strict) {
-        if (direction == null) {
-            return false
-        } else if (value == '>' && direction != Coordinate.right) {
-            return false
-        } else if (value == '<' && direction != Coordinate.left) {
-            return false
-        } else if (value == 'v' && direction != Coordinate.down) {
-            return false
-        } else if (value == '^' && direction != Coordinate.up) {
-            return false
-        }
-    }
-    return true
 }
