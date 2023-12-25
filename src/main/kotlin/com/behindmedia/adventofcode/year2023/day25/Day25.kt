c0315e8c9e760e8f675fb85aa8c133880a1bebd9
Werner Altewischer <werner.altewischer@gmail.com>
2023-12-25 10:03:01 +0100

    Code simplifications

--- src/main/kotlin/com/behindmedia/adventofcode/year2023/day25/Day25.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2023/day25/Day25.kt
@@ -3,26 +3,18 @@ package com.behindmedia.adventofcode.year2023.day25
 import com.behindmedia.adventofcode.common.*
 
 fun main() {
-    val connections = mutableMapOf<String, MutableSet<String>>()
-    parseLines("/2023/day25.txt") { line ->
-        val (source, rest) = line.split(":")
-        val destinations = rest.split(" ").filter { it.isNotBlank() }
-        connections.getOrPut(source) { mutableSetOf() }.addAll(destinations)
-        for (d in destinations) {
-            connections.getOrPut(d) { mutableSetOf() }.add(source)
-        }
-    }
+    val connections = parseInput(read("/2023/day25.txt"))
     val total = connections.size
     val counter = defaultMutableMapOf<Set<String>, Int> { 0 }
     for (start in connections.keys) {
-        reachAll(start = start, connections = connections, counter = counter)
+        reachAll(graph = connections, start = start, counter = counter)
     }
     val mostReached = counter.entries.sortedByDescending { it.value }
     for (i in 0 until mostReached.size) {
         for (j in i + 1 until mostReached.size) {
             for (k in j + 1 until mostReached.size) {
                 val prunedConnections = connections.removingEdges(mostReached[i].key, mostReached[j].key, mostReached[k].key)
-                val reached = reachAll(start = connections.keys.first(), connections = prunedConnections, counter = null)
+                val reached = reachAll(graph = prunedConnections, start = connections.keys.first(), counter = null)
                 if (reached < total) {
                     println(reached * (total - reached))
                     return
@@ -32,38 +24,38 @@ fun main() {
     }
 }
 
-private fun Map<String, Set<String>>.removingEdges(vararg edges: Set<String>): Map<String, Set<String>> {
-    val result = mutableMapOf<String, Set<String>>()
-    for ((k, v) in this) {
-        val edge = edges.firstOrNull { k in it }
-        if (edge == null) {
-            result[k] = v
-        } else {
-            val (first, last) = edge.toList()
-            when (k) {
-                first -> {
-                    result[k] = v - last
-                }
-                last -> {
-                    result[k] = v - first
-                }
-                else -> {
-                    error("Should never happen")
-                }
+private fun parseInput(string: String): Map<String, Set<String>> {
+    return string.split("\n").filter { it.isNotBlank() }.fold(mutableMapOf<String, MutableSet<String>>()) { connections, line ->
+        connections.apply {
+            val (source, rest) = line.split(":")
+            val destinations = rest.split(" ").filter { it.isNotBlank() }
+            connections.getOrPut(source) { mutableSetOf() }.addAll(destinations)
+            for (d in destinations) {
+                connections.getOrPut(d) { mutableSetOf() }.add(source)
             }
         }
     }
-    return result
 }
 
-private fun reachAll(start: String, connections: Map<String, Set<String>>, counter: DefaultMutableMap<Set<String>, Int>?): Int {
+private fun Map<String, Set<String>>.removingEdges(vararg edges: Set<String>): Map<String, Set<String>> {
+    return this.mapValues { (k, v) ->
+        val edge = edges.firstOrNull { k in it }
+        if (edge == null) {
+            v
+        } else {
+            v - (edge - k)
+        }
+    }
+}
+
+private fun reachAll(graph: Map<String, Set<String>>, start: String, counter: DefaultMutableMap<Set<String>, Int>?): Int {
     val seen = mutableSetOf<String>()
     val pending = ArrayDeque<Path<String>>()
     pending += Path(start, 0, null)
     seen += start
     while (pending.isNotEmpty()) {
         val current = pending.removeFirst()
-        val edges = connections[current.destination]!!
+        val edges = graph[current.destination]!!
         for (e in edges) {
             if (e in seen) continue
             seen += e
