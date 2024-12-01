a29975bf79e6620dfd8a29027b7254ec51021fdf
Werner Altewischer <werner.altewischer@gmail.com>
2024-12-01 16:37:25 +0100

    2024 day 1

--- src/main/kotlin/com/behindmedia/adventofcode/year2024/Day1.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2024/Day1.kt
@@ -5,7 +5,20 @@ import kotlin.math.*
 
 fun main() {
     val data = parseLines("/2024/day1.txt") { line ->
-        line
+        val (i, j) = line.splitNonEmpty(" ").map { it.toInt() }
+        i to j
     }
-    println(data)
+
+    val list1 = data.map { it.first }.sorted()
+    val list2 = data.map { it.second }.sorted()
+
+    // Part 1
+    println(list1.zip(list2).sumOf { (i, j) -> abs(i - j) })
+
+    // Part 2
+    val countMap = list2.fold(defaultMutableMapOf<Int, Int> { 0 }) { map, value ->
+        map[value]++
+        map
+    }
+    println(list1.sumOf { it * countMap[it] })
 }
