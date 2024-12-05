228274da8c719f770413ff25f5240b7ea1ba7093
Werner Altewischer <werner.altewischer@gmail.com>
2024-12-05 16:25:31 +0000

    Code improvements

--- src/main/kotlin/com/behindmedia/adventofcode/year2024/day5/Day5.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2024/day5/Day5.kt
@@ -2,21 +2,23 @@ package com.behindmedia.adventofcode.year2024.day5
 
 import com.behindmedia.adventofcode.common.*
 
+private typealias Rule = Pair<Int, Int>
+
 fun main() = timing {
     val data = read("/2024/day5.txt")
     val (firstSection, secondSection) = data.split("\n\n")
-    val rules = firstSection.splitNonEmpty("\n").map { line -> line.splitNonEmpty("|").map { it.toInt() }.let(Rule::invoke) }
-    val pages = secondSection.splitNonEmpty("\n").map { line -> line.splitNonEmpty(",").map { it.toInt() } }
-    val ruleMap = rules.groupBy { it.before }
+    val rules: List<Rule> = firstSection.splitNonEmpty("\n").map { line -> line.splitNonEmpty("|").map { it.toInt() }.toRule() }
+    val pagesList: List<List<Int>> = secondSection.splitNonEmpty("\n").map { line -> line.splitNonEmpty(",").map { it.toInt() } }
+    val ruleMap = rules.groupBy { it.first }
 
     var result1 = 0
     var result2 = 0
-    for (page in pages) {
-        if (page.isValidPage(ruleMap)) {
-            result1 += page[page.size / 2]
+    for (pages in pagesList) {
+        if (pages.isValid(ruleMap)) {
+            result1 += pages[pages.size / 2]
         } else {
-            val validPage = page.makeValidPage(ruleMap)
-            result2 += validPage[page.size / 2]
+            val validPage = pages.makeValid(ruleMap)
+            result2 += validPage[pages.size / 2]
         }
     }
 
@@ -27,11 +29,16 @@ fun main() = timing {
     println(result2)
 }
 
-private fun List<Int>.isValidPage(ruleMap: Map<Int, List<Rule>>): Boolean {
+private fun List<Int>.toRule(): Rule {
+    require(this.size == 2)
+    return Rule(this[0], this[1])
+}
+
+private fun List<Int>.isValid(ruleMap: Map<Int, List<Rule>>): Boolean {
     val seen = mutableSetOf<Int>()
     for (n in this) {
         val rules = ruleMap[n] ?: emptyList()
-        if (rules.any { seen.contains(it.after) }) {
+        if (rules.any { seen.contains(it.second) }) {
             return false
         }
         seen += n
@@ -39,10 +46,10 @@ private fun List<Int>.isValidPage(ruleMap: Map<Int, List<Rule>>): Boolean {
     return true
 }
 
-private fun List<Int>.makeValidPage(ruleMap: Map<Int, List<Rule>>): List<Int> {
+private fun List<Int>.makeValid(ruleMap: Map<Int, List<Rule>>): List<Int> {
     // Only select the applicable rules for this page: it is impossible to take all rules as they contain cycles!
     val allItems = this.toSet()
-    val applicableRules = allItems.flatMap { ruleMap[it] ?: emptyList() }.filter { it.after in allItems}
+    val applicableRules = allItems.flatMap { ruleMap[it] ?: emptyList() }.filter { it.second in allItems}
     val sortedPages = applicableRules.topologicallySortedPages()
     require(sortedPages.size == this.size) {
         "Expected all items to be present"
@@ -50,42 +57,35 @@ private fun List<Int>.makeValidPage(ruleMap: Map<Int, List<Rule>>): List<Int> {
     return sortedPages
 }
 
-private data class Rule(val before: Int, val after: Int) {
-    companion object {
-        operator fun invoke(items: List<Int>): Rule {
-            require(items.size == 2)
-            return Rule(items[0], items[1])
-        }
-    }
-}
-
 /**
  * Returns the topologically sorted pages under the conditions of these rules.
  */
 private fun List<Rule>.topologicallySortedPages(): List<Int> {
-    val dependencies = defaultMutableMapOf<Int, MutableSet<Int>>(putValueImplicitly = true) { mutableSetOf() }
+    val dependencies = defaultMutableMapOf<Int, MutableSet<Int>>(putValueImplicitly = true) {
+        mutableSetOf()
+    }
     val inDegrees = defaultMutableMapOf<Int, Int> { 0 }
     for (rule in this) {
-        if (dependencies[rule.before].add(rule.after)) {
-            inDegrees[rule.after]++
+        if (dependencies[rule.first].add(rule.second)) {
+            inDegrees[rule.second]++
         }
     }
     val pending = ArrayDeque<Int>()
     for (rule in this) {
-        if (inDegrees[rule.before] == 0) {
-            pending += rule.before
+        if (inDegrees[rule.first] == 0) {
+            pending += rule.first
         }
     }
-    require(pending.isNotEmpty()) { "Could not find a solution" }
+    require(pending.isNotEmpty()) {
+        "Recursion in rules: no solution found"
+    }
     val seen = linkedSetOf<Int>()
     while (pending.isNotEmpty()) {
         val next = pending.removeFirst()
-        if (seen.contains(next)) {
+        if (!seen.add(next)) {
             continue
         }
-        seen += next
-        val deps = dependencies[next]
-        for (dependency in deps) {
+        for (dependency in dependencies[next]) {
             val count = inDegrees[dependency]
             if (count == 1) {
                 inDegrees.remove(dependency)
@@ -97,4 +97,3 @@ private fun List<Rule>.topologicallySortedPages(): List<Int> {
     }
     return seen.toList()
 }
-
