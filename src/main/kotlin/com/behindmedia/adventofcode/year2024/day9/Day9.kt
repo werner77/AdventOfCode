1d70da5beee495b109ba471080a0eca8a93d90b4
Werner Altewischer <werner.altewischer@gmail.com>
2024-12-09 14:01:13 +0000

    Simplified code

--- src/main/kotlin/com/behindmedia/adventofcode/year2024/day9/Day9.kt
+++ src/main/kotlin/com/behindmedia/adventofcode/year2024/day9/Day9.kt
@@ -2,119 +2,96 @@ package com.behindmedia.adventofcode.year2024.day9
 
 import com.behindmedia.adventofcode.common.*
 import java.util.*
-import kotlin.math.*
 
 fun main() = timing {
     val data = read("/2024/day9.txt").trim()
-
     println(part1(data))
     println(part2(data))
 }
 
-data class FileInfo(val id: Int, val length: Int)
-
 private fun part1(data: String): Long {
-    val fileMap = getFileMap(data)
-    var pos = 0
-    while (true) {
-        val (filePos, file) = fileMap.lastOrNull { it.value.id >= 0 && it.key >= pos } ?: break
-        val (gapPos, gap) = fileMap.removeFirstOrNull { it.key < filePos && it.value.id == -1 } ?: break
-        if (gap.length >= file.length) {
-            // Completely move file
-            fileMap.remove(filePos)
-            fileMap[gapPos] = file
-            val sizeLeft = gap.length - file.length
-            if (sizeLeft > 0) {
-                fileMap[gapPos + file.length] = FileInfo(id = -1, length = sizeLeft)
-            }
-        } else {
-            // Partly move file
-            fileMap[filePos] = FileInfo(file.id, file.length - gap.length)
-            fileMap[gapPos] = FileInfo(file.id, gap.length)
-        }
-        pos = gapPos + min(gap.length, file.length)
+    var size = 0
+    for ((i, c) in data.withIndex()) {
+        size += c.digitToInt()
     }
-
-    return fileMap.checksum()
-}
-
-private fun getFileMap(data: String): TreeMap<Int, FileInfo> {
-    val fileMap = TreeMap<Int, FileInfo>()
+    val array = IntArray(size) { -1 }
     var pos = 0
     for ((i, c) in data.withIndex()) {
-        val id = if (i % 2 == 0) {
-            i / 2
-        } else {
-            -1
-        }
         val length = c.digitToInt()
-        fileMap[pos] = FileInfo(id, length)
+        if (i % 2 == 0) {
+            val id = i / 2
+            for (j in 0 until length) {
+                array[pos + j] = id
+            }
+        }
         pos += length
     }
-    return fileMap
-}
 
-
-private fun part2(data: String): Long {
-    val fileMap = getFileMap(data)
-    val seen = mutableSetOf<Int>()
+    var head = 0
+    var tail = array.size - 1
     while (true) {
-        val (filePos, file) = fileMap.lastOrNull { it.value.id >= 0 && seen.add(it.value.id) } ?: break
-
-        // Check if there is a gap to move to, if not just ignore and continue with next
-        val (gapPos, gap) = fileMap.removeFirstOrNull { it.key < filePos && it.value.id == -1 && it.value.length >= file.length } ?: continue
-
-        fileMap.remove(filePos)
-        fileMap[gapPos] = file
-        val sizeLeft = gap.length - file.length
-        if (sizeLeft > 0) {
-            fileMap[gapPos + file.length] = FileInfo(id = -1, length = sizeLeft)
+        while (array[head] >= 0) {
+            head++
         }
+        while (array[tail] == -1) {
+            tail--
+        }
+        if (head >= tail) break
+        array[head] = array[tail]
+        array[tail] = -1
+        head++
+        tail--
     }
-    return fileMap.checksum()
-}
-
-private fun TreeMap<Int, FileInfo>.checksum(): Long {
     var sum = 0L
-    for ((pos, file) in this) {
-        if (file.id == -1) continue
-        for (i in 0 until file.length) {
-            sum += (pos + i).toLong() * file.id.toLong()
-        }
+    for (i in array.indices) {
+        val id = array[i]
+        if (id < 0) continue
+        sum += id.toLong() * i.toLong()
     }
     return sum
 }
 
-private fun TreeMap<Int, FileInfo>.removeFirstOrNull(where: (Map.Entry<Int, FileInfo>) -> Boolean): Pair<Int, FileInfo>? {
-    val iterator = this.iterator()
-    while (iterator.hasNext()) {
-        val next = iterator.next()
-        if (where(next)) {
-            val result = next.key to next.value
-            iterator.remove()
-            return result
+private fun part2(data: String): Long {
+    val gaps = TreeMap<Int, Int>()
+    val files = mutableMapOf<Int, Pair<Int, Int>>()
+    var pos = 0
+    for ((i, c) in data.withIndex()) {
+        val length = c.digitToInt()
+        if (i % 2 == 0) {
+            // File
+            val id = i / 2
+            files[id] = pos to length
+        } else {
+            // Gap
+            gaps[pos] = length
         }
+        pos += length
     }
-    return null
-}
+    val maxId = files.keys.max()
+    for (id in maxId downTo 0) {
+        val (filePos, fileLength) = files[id] ?: error("No file with id $id found")
 
-private fun TreeMap<Int, FileInfo>.lastOrNull(where: (Map.Entry<Int, FileInfo>) -> Boolean): Pair<Int, FileInfo>? {
-    val iterator = this.descendingMap().iterator()
-    while (iterator.hasNext()) {
-        val next = iterator.next()
-        if (where(next)) {
-            return next.key to next.value
-        }
-    }
-    return null
-}
+        // Find first fitting position
+        for ((gapPos, gapLength) in gaps) {
+            if (gapPos >= filePos) break
+            if (gapLength < fileLength) continue
 
-private fun TreeMap<Int, FileInfo>.asString(): String {
-    val builder = StringBuilder()
-    for ((_, file) in this) {
-        for (i in 0 until file.length) {
-            builder.append(if (file.id >= 0 ) file.id else ".")
+            files[id] = gapPos to fileLength
+
+            // Remove gap
+            gaps.remove(gapPos)
+            if (gapLength > fileLength) {
+                gaps[gapPos + fileLength] = gapLength - fileLength
+            }
+            break
         }
     }
-    return builder.toString()
+    var result = 0L
+    for ((id, info) in files) {
+        val (p, l) = info
+        for (i in p until p + l) {
+            result += id.toLong() * i.toLong()
+        }
+    }
+    return result
 }
